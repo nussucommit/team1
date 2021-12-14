94a2cc08ce6a4d388dacd63c7c2116863120f5f8
siyuancheng178 <siyuanc6794@gmail.com>
2021-12-14 17:51:18 +0800

    Update index.js

--- src/PostList/index.js
+++ src/PostList/index.js
@@ -5,13 +5,13 @@ import React from 'react'
 const listData = [];
 for (let i = 0; i < 23; i++) {
     listData.push({
-        href: 'https://ant.design',
-        title: `ant design part ${i}`,
+        href: '#',
+        title: `test title ${i}`,
         avatar: 'https://joeschmoe.io/api/v1/random',
         description:
-            'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+            'Created by developer on Dec 8th, 2021',
         content:
-            'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+            'Testing.',
     });
 }
 
@@ -30,14 +30,13 @@ const PostList = () => (
             onChange: page => {
                 console.log(page);
             },
-            pageSize: 3,
+            pageSize: 10,
         }}
         dataSource={listData}
         renderItem={item => (
             <List.Item
                 key={item.title}
                 actions={[
-                    <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
                     <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
                     <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
                 ]}
